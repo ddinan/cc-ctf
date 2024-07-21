6d2ba6c4a340177dc5ff5b7d5ef659699ffc2037
Derek <19647340+ddinan@users.noreply.github.com>
2024-07-21 18:35:14 +1000

    Set up project structure

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MCGalaxy;
+
+namespace CTF
+{
+    public class CTF : Plugin
+    {
+        public override string name { get { return "CTF"; } }
+        public override string MCGalaxy_Version { get { return "1.9.4.9"; } }
+        public override string creator { get { return "Venk"; } }
+
+        public override void Load(bool startup)
+        {
+        }
+
+        public override void Unload(bool shutdown)
+        {
+        }
+
+        public override void Help(Player p)
+        {
+        }
+    }
+}
